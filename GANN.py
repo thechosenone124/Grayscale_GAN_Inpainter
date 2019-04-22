9b9a1053e3f499c01da3651bbd65b8a6a50f23ba
Bryan <thechosenone124@gmail.com>
2019-04-22 03:23:19 -0700

    initializing repository

---
+++ GANN.py
@@ -0,0 +1,290 @@
+'''
+DCGAN on MNIST using Keras
+Author: Rowel Atienza
+Project: https://github.com/roatienza/Deep-Learning-Experiments
+Dependencies: tensorflow 1.0 and keras 2.0
+Usage: python3 dcgan_mnist.py
+'''
+
+import numpy as np
+import time
+import gc
+import random_masks
+from keras.preprocessing.image import ImageDataGenerator
+from copy import deepcopy
+from tensorflow.examples.tutorials.mnist import input_data
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Reshape
+from keras.layers import Conv2D, Conv2DTranspose, UpSampling2D
+from keras.layers import LeakyReLU, Dropout
+from keras.layers import BatchNormalization
+from keras.optimizers import Adam, RMSprop
+
+import matplotlib.pyplot as plt
+
+from MaskGenerator import MaskGenerator
+
+#SETTINGS
+
+TRAIN_DIR = r"~\\Desktop\\TrainingImages\\512px\\0001"
+VAL_DIR = r"~\\Desktop\\TrainingImages\\512px\\0002"
+TEST_DIR = r"~\\Desktop\\TrainingImages\\512px\\0003"
+BATCH_SIZE = 256
+class AugmentingDataGenerator(ImageDataGenerator):
+    #Keras' ImageDataGenerator's flow from directory generates batches of augmented images
+    #We need masks and images together, so this wraps ImageDataGenerator
+    def flow_from_directory(self, directory, mask_generator, *args, **kwargs):
+        generator = super().flow_from_directory(directory, class_mode=None, *args, **kwargs)        
+        seed = None if 'seed' not in kwargs else kwargs['seed']
+        while True:
+            
+            # Get augmentend image samples
+            ori = next(generator)
+
+            # Get masks for each image sample            
+            mask = np.stack([
+                mask_generator.sample(seed)
+                for _ in range(ori.shape[0])], axis=0
+            )
+
+            # Apply masks to all image sample
+            # Because we want to generate what is here, so we dont want the model to know
+            masked = deepcopy(ori)
+            masked[mask==0] = 1
+
+            # Yield ([masked_image, mask],  ori) training batches
+            # print(masked.shape, ori.shape)
+            gc.collect()
+            yield [masked, mask], ori
+
+class ElapsedTimer(object):
+    def __init__(self):
+        self.start_time = time.time()
+    def elapsed(self,sec):
+        if sec < 60:
+            return str(sec) + " sec"
+        elif sec < (60 * 60):
+            return str(sec / 60) + " min"
+        else:
+            return str(sec / (60 * 60)) + " hr"
+    def elapsed_time(self):
+        print("Elapsed: %s " % self.elapsed(time.time() - self.start_time) )
+
+class DCGAN(object):
+    def __init__(self, img_rows=512, img_cols=512, channel=1):
+
+        self.img_rows = img_rows
+        self.img_cols = img_cols
+        self.channel = channel
+        self.D = None   # discriminator
+        self.G = None   # generator
+        self.AM = None  # adversarial model
+        self.DM = None  # discriminator model
+
+    # (W−F+2P)/S+1
+    def discriminator(self):
+        if self.D:
+            return self.D
+        self.D = Sequential()
+        depth = 64
+        dropout = 0.4
+        # In: 28 x 28 x 1, depth = 1
+        # Out: 14 x 14 x 1, depth=64
+        input_shape = (self.img_rows, self.img_cols, self.channel)
+        self.D.add(Conv2D(depth*1, 5, strides=2, input_shape=input_shape,\
+            padding='same'))
+        self.D.add(LeakyReLU(alpha=0.2))
+        self.D.add(Dropout(dropout))
+
+        self.D.add(Conv2D(depth*2, 5, strides=2, padding='same'))
+        self.D.add(LeakyReLU(alpha=0.2))
+        self.D.add(Dropout(dropout))
+
+        self.D.add(Conv2D(depth*4, 5, strides=2, padding='same'))
+        self.D.add(LeakyReLU(alpha=0.2))
+        self.D.add(Dropout(dropout))
+
+        self.D.add(Conv2D(depth*8, 5, strides=1, padding='same'))
+        self.D.add(LeakyReLU(alpha=0.2))
+        self.D.add(Dropout(dropout))
+
+        # Out: 1-dim probability
+        self.D.add(Flatten())
+        self.D.add(Dense(1))
+        self.D.add(Activation('sigmoid'))
+        self.D.summary()
+        return self.D
+    #Partial convolutional generator
+    def generator(self):
+        if self.G:
+            return self.G
+        
+        return PConvUnet()
+
+    def discriminator_model(self):
+        if self.DM:
+            return self.DM
+        optimizer = RMSprop(lr=0.0002, decay=6e-8)
+        self.DM = Sequential()
+        self.DM.add(self.discriminator())
+        self.DM.compile(loss='binary_crossentropy', optimizer=optimizer,\
+            metrics=['accuracy'])
+        return self.DM
+
+    def adversarial_model(self):
+        if self.AM:
+            return self.AM
+        optimizer = RMSprop(lr=0.0001, decay=3e-8)
+        self.AM = Sequential()
+        self.AM.add(self.generator())
+        self.AM.add(self.discriminator())
+        self.AM.compile(loss='binary_crossentropy', optimizer=optimizer,\
+            metrics=['accuracy'])
+        return self.AM
+
+class MNIST_DCGAN(object):
+    def __init__(self):
+        self.img_rows = 512
+        self.img_cols = 512
+        self.channel = 1
+        #set up input
+        # Create training generator
+        train_datagen = AugmentingDataGenerator(  
+            rotation_range=10,
+            width_shift_range=0.1,
+            height_shift_range=0.1,
+            rescale=1./255,
+            horizontal_flip=True
+        )
+        train_generator = train_datagen.flow_from_directory(
+            TRAIN_DIR, 
+            MaskGenerator(512, 512, 1),
+            target_size=(512, 512), 
+            batch_size=BATCH_SIZE
+        )
+
+        # Create validation generator
+        val_datagen = AugmentingDataGenerator(rescale=1./255)
+        val_generator = val_datagen.flow_from_directory(
+            VAL_DIR, 
+            MaskGenerator(512, 512, 1), 
+            target_size=(512, 512), 
+            batch_size=BATCH_SIZE, 
+            classes=['val'], 
+            seed=42
+        )
+
+        # Create testing generator
+        test_datagen = AugmentingDataGenerator(rescale=1./255)
+        test_generator = test_datagen.flow_from_directory(
+            TEST_DIR, 
+            MaskGenerator(512, 512, 1), 
+            target_size=(512, 512), 
+            batch_size=BATCH_SIZE, 
+            seed=42
+        )
+        #yep
+        
+        #Display some sample images
+        # Pick out an example
+        test_data = next(test_generator)
+        (masked, mask), ori = test_data
+
+       # # Show side by side
+       # for i in range(len(ori)):
+       # _, axes = plt.subplots(1, 3, figsize=(20, 5))
+       # axes[0].imshow(masked[i,:,:,:])
+       # axes[1].imshow(mask[i,:,:,:] * 1.)
+       # axes[2].imshow(ori[i,:,:,:])
+       # plt.show()
+       # #Not the correct trainign dataset
+        # self.x_train = input_data.read_data_sets("mnist",\
+        	# one_hot=True).train.images
+        # self.x_train = self.x_train.reshape(-1, self.img_rows,\
+        	# self.img_cols, 1).astype(np.float32)
+
+        self.DCGAN = DCGAN()
+        self.discriminator =  self.DCGAN.discriminator_model()
+        self.adversarial = self.DCGAN.adversarial_model()
+        self.generator = self.DCGAN.generator()
+        
+    def plot_callback(model):
+        """Called at the end of each epoch, displaying our previous test images,
+        as well as their masked predictions and saving them to disk"""
+        
+        # Get samples & Display them        
+        pred_img = model.predict([masked, mask])
+        pred_time = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+
+        # Clear current output and display test images
+        for i in range(len(ori)):
+            _, axes = plt.subplots(1, 3, figsize=(20, 5))
+            axes[0].imshow(masked[i,:,:,:])
+            axes[1].imshow(pred_img[i,:,:,:] * 1.)
+            axes[2].imshow(ori[i,:,:,:])
+            axes[0].set_title('Masked Image')
+            axes[1].set_title('Predicted Image')
+            axes[2].set_title('Original Image')
+                    
+            plt.savefig(r'data/test_samples/img_{}_{}.png'.format(i, pred_time))
+            plt.close()
+    
+    def train(self, train_steps=2000, batch_size=256, save_interval=0):
+        noise_input = None
+        if save_interval>0:
+            noise_input = np.random.uniform(-1.0, 1.0, size=[16, 100])
+        for i in range(train_steps):
+            images_train = self.x_train[np.random.randint(0,
+                self.x_train.shape[0], size=batch_size), :, :, :]
+            noise = np.random.uniform(-1.0, 1.0, size=[batch_size, 100])
+            images_fake = self.generator.predict(noise)
+            x = np.concatenate((images_train, images_fake))
+            y = np.ones([2*batch_size, 1])
+            y[batch_size:, :] = 0
+            d_loss = self.discriminator.train_on_batch(x, y)
+
+            y = np.ones([batch_size, 1])
+            noise = np.random.uniform(-1.0, 1.0, size=[batch_size, 100])
+            a_loss = self.adversarial.train_on_batch(noise, y)
+            log_mesg = "%d: [D loss: %f, acc: %f]" % (i, d_loss[0], d_loss[1])
+            log_mesg = "%s  [A loss: %f, acc: %f]" % (log_mesg, a_loss[0], a_loss[1])
+            print(log_mesg)
+            if save_interval>0:
+                if (i+1)%save_interval==0:
+                    self.plot_images(save2file=True, samples=noise_input.shape[0],\
+                        noise=noise_input, step=(i+1))
+
+    def plot_images(self, save2file=False, fake=True, samples=16, noise=None, step=0):
+        filename = 'mnist.png'
+        if fake:
+            if noise is None:
+                noise = np.random.uniform(-1.0, 1.0, size=[samples, 100])
+            else:
+                filename = "mnist_%d.png" % step
+            images = self.generator.predict(noise)
+        else:
+            i = np.random.randint(0, self.x_train.shape[0], samples)
+            images = self.x_train[i, :, :, :]
+
+        plt.figure(figsize=(10,10))
+        for i in range(images.shape[0]):
+            plt.subplot(4, 4, i+1)
+            image = images[i, :, :, :]
+            image = np.reshape(image, [self.img_rows, self.img_cols])
+            plt.imshow(image, cmap='gray')
+            plt.axis('off')
+        plt.tight_layout()
+        if save2file:
+            plt.savefig(filename)
+            plt.close('all')
+        else:
+            plt.show()
+
+if __name__ == '__main__':
+    mnist_dcgan = MNIST_DCGAN()
+    #timer = ElapsedTimer()
+    #mnist_dcgan.train(train_steps=10000, batch_size=256, save_interval=500)
+    #timer.elapsed_time()
+    #mnist_dcgan.plot_images(fake=True)
+    #mnist_dcgan.plot_images(fake=False, save2file=True)
---
+++ pconv_model.py
@@ -0,0 +1,151 @@
+import os
+import sys
+import numpy as np
+from datetime import datetime
+
+import tensorflow as tf
+from keras.models import Model
+from keras.models import load_model
+from keras.optimizers import Adam
+from keras.layers import Input, Conv2D, UpSampling2D, Dropout, LeakyReLU, BatchNormalization, Activation, Lambda
+from keras.layers.merge import Concatenate
+from keras.applications import VGG16
+from keras import backend as K
+from keras.utils.multi_gpu_utils import multi_gpu_model
+
+from libs.pconv_layer import PConv2D
+
+# source: https://github.com/MathiasGruber/PConv-Keras
+
+class PConvUnet(object):
+        
+        def __init__(self, img_rows=512, img_cols=512, weight_filepath=None):
+        """Create the PConvUnet. If variable image size, set img_rows and img_cols to None
+        
+        Args:
+            img_rows (int): image height.
+            img_cols (int): image width.
+            weight_filepath: filepath containing weights
+        """
+        
+        # Settings
+        self.weight_filepath = weight_filepath
+        self.img_rows = img_rows
+        self.img_cols = img_cols
+        assert self.img_rows >= 256, 'Height must be >256 pixels'
+        assert self.img_cols >= 256, 'Width must be >256 pixels'
+
+        # Set current epoch
+        self.current_epoch = 0
+        
+        # Create UNet-like model
+        self.model = self.build_pconv_unet()
+        
+    #Modified for grayscale images and masks
+    def build_pconv_unet(self, train_bn=True):      
+
+        # INPUTS
+        inputs_img = Input((self.img_rows, self.img_cols, 1), name='inputs_img')
+        inputs_mask = Input((self.img_rows, self.img_cols, 1), name='inputs_mask')
+        
+        # ENCODER
+        def encoder_layer(img_in, mask_in, filters, kernel_size, bn=True):
+            conv, mask = PConv2D(filters, kernel_size, strides=2, padding='same')([img_in, mask_in])
+            if bn:
+                conv = BatchNormalization(name='EncBN'+str(encoder_layer.counter))(conv, training=train_bn)
+            conv = Activation('relu')(conv)
+            encoder_layer.counter += 1
+            return conv, mask
+        encoder_layer.counter = 0
+        
+        e_conv1, e_mask1 = encoder_layer(inputs_img, inputs_mask, 64, 7, bn=False)
+        e_conv2, e_mask2 = encoder_layer(e_conv1, e_mask1, 128, 5)
+        e_conv3, e_mask3 = encoder_layer(e_conv2, e_mask2, 256, 5)
+        e_conv4, e_mask4 = encoder_layer(e_conv3, e_mask3, 512, 3)
+        e_conv5, e_mask5 = encoder_layer(e_conv4, e_mask4, 512, 3)
+        e_conv6, e_mask6 = encoder_layer(e_conv5, e_mask5, 512, 3)
+        e_conv7, e_mask7 = encoder_layer(e_conv6, e_mask6, 512, 3)
+        e_conv8, e_mask8 = encoder_layer(e_conv7, e_mask7, 512, 3)
+        
+        # DECODER
+        def decoder_layer(img_in, mask_in, e_conv, e_mask, filters, kernel_size, bn=True):
+            up_img = UpSampling2D(size=(2,2))(img_in)
+            up_mask = UpSampling2D(size=(2,2))(mask_in)
+            concat_img = Concatenate(axis=3)([e_conv,up_img])
+            concat_mask = Concatenate(axis=3)([e_mask,up_mask])
+            conv, mask = PConv2D(filters, kernel_size, padding='same')([concat_img, concat_mask])
+            if bn:
+                conv = BatchNormalization()(conv)
+            conv = LeakyReLU(alpha=0.2)(conv)
+            return conv, mask
+            
+        d_conv9, d_mask9 = decoder_layer(e_conv8, e_mask8, e_conv7, e_mask7, 512, 3)
+        d_conv10, d_mask10 = decoder_layer(d_conv9, d_mask9, e_conv6, e_mask6, 512, 3)
+        d_conv11, d_mask11 = decoder_layer(d_conv10, d_mask10, e_conv5, e_mask5, 512, 3)
+        d_conv12, d_mask12 = decoder_layer(d_conv11, d_mask11, e_conv4, e_mask4, 512, 3)
+        d_conv13, d_mask13 = decoder_layer(d_conv12, d_mask12, e_conv3, e_mask3, 256, 3)
+        d_conv14, d_mask14 = decoder_layer(d_conv13, d_mask13, e_conv2, e_mask2, 128, 3)
+        d_conv15, d_mask15 = decoder_layer(d_conv14, d_mask14, e_conv1, e_mask1, 64, 3)
+        d_conv16, d_mask16 = decoder_layer(d_conv15, d_mask15, inputs_img, inputs_mask, 3, 3, bn=False)
+        outputs = Conv2D(3, 1, activation = 'sigmoid', name='outputs_img')(d_conv16)
+        
+        # Setup the model inputs / outputs
+        model = Model(inputs=[inputs_img, inputs_mask], outputs=outputs)
+
+        return model, inputs_mask    
+
+    #Do not run this. The generator has no loss function and should not be compiled    
+    def compile_pconv_unet(self, model, inputs_mask, lr=0.0002):
+        model.compile(
+            optimizer = Adam(lr=lr),
+            #We don't need this, we have discriminator loss
+            #loss=self.loss_total(inputs_mask),
+            metrics=[self.PSNR]
+        )
+    
+    #This is for training the generator the conventional way, we have our GANN training
+    def fit_generator(self, generator, *args, **kwargs):
+        """Fit the U-Net to a (images, targets) generator
+
+        Args:
+            generator (generator): generator supplying input image & mask, as well as targets.
+            *args: arguments to be passed to fit_generator
+            **kwargs: keyword arguments to be passed to fit_generator
+        """
+        self.model.fit_generator(
+            generator,
+            *args, **kwargs
+        )
+        
+    def predict(self, sample):
+        """Run prediction using this model"""
+        return self.model.predict(sample)
+        
+    def summary(self):
+        """Get summary of the UNet model"""
+        print(self.model.summary())
+
+    def save(self):        
+        self.model.save_weights(self.current_weightfile())
+        
+    def load(self, filepath, train_bn=True, lr=0.0002):
+
+        # Create UNet-like model
+        self.model, inputs_mask = self.build_pconv_unet(train_bn)
+        self.compile_pconv_unet(self.model, inputs_mask, lr) 
+
+        # Load weights into model
+        epoch = int(os.path.basename(filepath).split('.')[1].split('-')[0])
+        assert epoch > 0, "Could not parse weight file. Should include the epoch"
+        self.current_epoch = epoch
+        self.model.load_weights(filepath)
+
+    @staticmethod
+    def current_timestamp():
+        return datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    
+    # Prediction functions
+    ######################
+    def predict(self, sample, **kwargs):
+        """Run prediction using this model"""
+        return self.model.predict(sample, **kwargs)
